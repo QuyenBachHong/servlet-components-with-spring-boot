983fbab8c12ac125d9707ab4c43f7d45eeb7224f
QuyenBachHong <javacode123456789@gmail.com>
2024-09-15 20:54:34 +0700

    jakarta filter

--- spring-custom-filter/src/main/java/me/quyen/filters/CustomHeadersLoggingFilter.java
+++ spring-custom-filter/src/main/java/me/quyen/filters/CustomHeadersLoggingFilter.java
@@ -13,19 +13,27 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.awt.*;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
 @Component
 @Slf4j
 public class CustomHeadersLoggingFilter extends OncePerRequestFilter {
     public static int count = 0;
+    public static int headerCount = 0;
     @Override
     protected void doFilterInternal(
             HttpServletRequest request
             , HttpServletResponse response
             , FilterChain filterChain) throws ServletException, IOException {
-
+        Collections.list(request.getHeaderNames())
+                .forEach(
+                        h -> log.info("{},header-name:{}, header-value:{}"
+                                ,++CustomHeadersLoggingFilter.headerCount,h,Collections.list(request.getHeaders(h)))
+                );
+        // allow request pass this filer
+        filterChain.doFilter(request,response);
     }
-
 
     @Override
     protected String getAlreadyFilteredAttributeName() {
@@ -54,39 +62,52 @@ public class CustomHeadersLoggingFilter extends OncePerRequestFilter {
 
     @Override
     public FilterConfig getFilterConfig() {
-        return super.getFilterConfig();
+        FilterConfig filterConfig = CustomHeadersLoggingFilter.super.getFilterConfig();
+        Utils.classInfo(filterConfig.getClass(),log);
+        return filterConfig;
     }
 
 
     @Override
     protected String getFilterName() {
-        return super.getFilterName();
+        String filterName = CustomHeadersLoggingFilter.super.getFilterName();
+        Utils.printInfo("me.quyen.filters.CustomHeadersLoggingFilter.getFilterName()"
+                            ,filterName,log);
+        return filterName;
     }
 
     @Override
     protected ServletContext getServletContext() {
-        return super.getServletContext();
+        ServletContext servletContext = CustomHeadersLoggingFilter.super.getServletContext();
+        Utils.classInfo(servletContext.getClass(),log);
+        return servletContext;
     }
 
 
     @Override
     protected void initBeanWrapper(BeanWrapper bw) throws BeansException {
-        super.initBeanWrapper(bw);
+        Utils.printInfo(
+                "me.quyen.filters.CustomHeadersLoggingFilter.initBeanWrapper(BeanWrapper) throws BeansException"
+                        ,"__null",log);
+        Utils.classInfo(bw.getClass(),log);
+        CustomHeadersLoggingFilter.super.initBeanWrapper(bw);
     }
 
 
     @Override
     protected void initFilterBean() throws ServletException {
-        super.initFilterBean();
+        Utils.printInfo(
+                "me.quyen.filters.CustomHeadersLoggingFilter.initFilterBean() throws BeansException"
+                ,"__null",log);
+        CustomHeadersLoggingFilter.super.initFilterBean();
     }
 
     @Override
     public void destroy() {
+        Utils.printInfo(
+                "me.quyen.filters.CustomHeadersLoggingFilter.destroy() throws BeansException"
+                ,"__null",log);
         CustomHeadersLoggingFilter.super.destroy();
     }
-
-
-
-
 
 }
